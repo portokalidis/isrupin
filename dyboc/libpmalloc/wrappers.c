8244d81783aef4ba6d69d27786c7b4d68d360fc0
G Portokalidis <gportoka@stevens.edu>
2014-09-07 00:47:46 -0400

    Test new posix_memalign.

---
+++ dyboc/libpmalloc/wrappers.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+#include "pmalloc.h"
+
+
+void *malloc(size_t size)
+{
+	fprintf(stderr, "malloc UNIMPLEMENTED!\n");
+	return NULL;
+}
+
+
+void *calloc(size_t nmemb, size_t size)
+{
+	fprintf(stderr, "calloc UNIMPLEMENTED!\n");
+	return NULL;
+}
+
+void free(void *ptr)
+{
+	if (ptr)
+		pfree(ptr);
+}
+
+void *realloc(void *ptr, size_t size)
+{
+	fprintf(stderr, "realloc UNIMPLEMENTED!\n");
+	return NULL;
+}
+
+
+int posix_memalign(void **memptr, size_t alignment, size_t size)
+{
+	/* We return NULL without an error */
+	if (size == 0) {
+		*memptr = NULL;
+		return 0;
+	}
+
+	return pmemalign(memptr, alignment, size);
+}
